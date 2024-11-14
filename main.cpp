bc84a352d9ccf27382aa2f8b1625a76437ed9f86
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-11-14 08:51:14 +0100

    log version number of software at start

--- main.cpp
+++ main.cpp
@@ -1,42 +1,51 @@
 #include <modbus/modbus.h>
 #include <iostream>
 
-int main() {
-    // Create a new RTU context
-    modbus_t *ctx = modbus_new_rtu("/dev/ttyUSB0", 57600, 'N', 8, 2);
-    if (ctx == nullptr) {
-        std::cerr << "Unable to create the libmodbus context" << std::endl;
-        return -1;
-    }
+#include "cg_version.h"
 
-    // Set the Modbus slave ID
-    modbus_set_slave(ctx, 1);
+int main()
+{
+  std::cout << "Cellulite GUI Version: " << CG_VERSION << std::endl;
 
-    // Connect to the Modbus server
-    if (modbus_connect(ctx) == -1) {
-        std::cerr << "Connection failed: " << modbus_strerror(errno) << std::endl;
-        modbus_free(ctx);
-        return -1;
-    }
+  // Create a new RTU context
+  modbus_t* ctx = modbus_new_rtu( "/dev/ttyUSB0", 57600, 'N', 8, 2 );
+  if(ctx == nullptr)
+  {
+    std::cerr << "Unable to create the libmodbus context" << std::endl;
+    return -1;
+  }
 
-    // Read 5 holding registers starting from address 0
-    uint16_t tab_reg[5];
-    int rc = modbus_read_registers(ctx, 101, 5, tab_reg);
-    if (rc == -1) {
-        std::cerr << "Failed to read registers: " << modbus_strerror(errno) << std::endl;
-        modbus_close(ctx);
-        modbus_free(ctx);
-        return -1;
-    }
+  // Set the Modbus slave ID
+  modbus_set_slave( ctx, 1 );
 
-    // Print the read registers
-    for (int i = 0; i < 5; ++i) {
-        std::cout << "Register " << i << ": " << tab_reg[i] << std::endl;
-    }
+  // Connect to the Modbus server
+  if(modbus_connect( ctx ) == -1)
+  {
+    std::cerr << "Connection failed: " << modbus_strerror( errno ) << std::endl;
+    modbus_free( ctx );
+    return -1;
+  }
 
-    // Close the connection and free the context
-    modbus_close(ctx);
-    modbus_free(ctx);
+  // Read 5 holding registers starting from address 0
+  uint16_t tab_reg[ 5 ];
+  int rc = modbus_read_registers( ctx, 101, 5, tab_reg );
+  if(rc == -1)
+  {
+    std::cerr << "Failed to read registers: " << modbus_strerror( errno ) << std::endl;
+    modbus_close( ctx );
+    modbus_free( ctx );
+    return -1;
+  }
 
-    return 0;
+  // Print the read registers
+  for(int i = 0; i < 5; ++i)
+  {
+    std::cout << "Register " << i << ": " << tab_reg[ i ] << std::endl;
+  }
+
+  // Close the connection and free the context
+  modbus_close( ctx );
+  modbus_free( ctx );
+
+  return 0;
 }
