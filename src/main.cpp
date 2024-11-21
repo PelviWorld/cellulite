caf1d4b02a0a0fdc58a8971a0c8d790d2890ea44
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-11-21 12:36:42 +0100

    used the moveToPosition function in main to check functionality

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@ int main()
 {
   std::cout << "Cellulite GUI Version: " << CG_VERSION << std::endl;
   auto laingController = LaingController( "/dev/ttyUSB0" );
+  laingController.moveToPosition( 2 );
+  laingController.moveToPosition( 4 );
+  laingController.moveToPosition( 1 );
+  laingController.moveToPosition( 3 );
 
   return 0;
 }
