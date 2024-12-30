1c1a55938da9d0dbbbb350391a88c548a137dae2
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-30 10:41:49 +0100

    adding ini file to project and output its values to log file

--- gui/src/celluliteapp.cpp
+++ gui/src/celluliteapp.cpp
@@ -1,8 +1,39 @@
 #include "celluliteapp.h"
+#include "INIReader.h"
 #include "celluliteframe.h"
+
+#include <fstream>
+
+std::ofstream logFile;
+
+void RedirectIOToLogFile( const std::string& logFileName )
+{
+  logFile.open( logFileName );
+  if( logFile.is_open() )
+  {
+    std::cout.rdbuf( logFile.rdbuf() );
+    std::cerr.rdbuf( logFile.rdbuf() );
+  }
+  else
+  {
+    std::cerr << "Failed to open log file: " << logFileName << std::endl;
+  }
+}
 
 bool CelluliteApp::OnInit()
 {
+  RedirectIOToLogFile( "cellulite.log" );
+  if( const INIReader reader( "cellulite.ini" ); reader.ParseError() != 0 )
+  {
+    std::cout << "Can't load 'cellulite.ini'\n" << std::endl;
+  }
+  else
+  {
+    std::cout << "Config value: " << reader.Get( "DEVICES", "one", "NOTHING" ) << std::endl;
+    std::cout << "Config value: " << reader.Get( "DEVICES", "two", "NOTHING" ) << std::endl;
+    std::cout << "Config value: " << reader.Get( "AXIS", "width", "NOTHING" ) << std::endl;
+    std::cout << "Config value: " << reader.Get( "AXIS", "height", "NOTHING" ) << std::endl;
+  }
   auto* frame = new CelluliteFrame();
   frame->Show( true );
   return true;
